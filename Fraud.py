3bbddffc1f63ea6535f11eb937cc3ba18eda88da
dizshenoy <89087808+dizshenoy@users.noreply.github.com>
2022-09-24 16:27:12 +0530

    Add files via upload

---
+++ Fraud.py
@@ -0,0 +1,99 @@
+# import the necessary packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import timeit
+import warnings
+warnings.filterwarnings("ignore")
+import streamlit as st
+
+st.title('Fraud Detection in online Payment models!')
+
+# Load the dataset from the csv file using pandas
+df=st.cache(pd.read_csv)('credit card fraud.csv')
+df = df.sample(frac=0.1, random_state = 48)
+
+    #Obtaining X (features) and y (labels)
+X=df.drop(['Class'], axis=1)
+y=df.Class
+#Split the data into training and testing sets
+from sklearn.model_selection import train_test_split
+size = st.sidebar.slider('Test Set Size', min_value=0.2, max_value=0.4)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = size, random_state = 42)
+#Print shape of train and test sets
+
+    #Import classification models and metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier,ExtraTreesClassifier
+from sklearn.model_selection import cross_val_score
+logreg=LogisticRegression()
+svm=SVC()
+knn=KNeighborsClassifier()
+etree=ExtraTreesClassifier(random_state=42)
+rforest=RandomForestClassifier(random_state=42)
+
+#Feature selection through feature importance
+@st.cache
+def feature_sort(model,X_train,y_train):
+    #feature selection
+    mod=model
+    # fit the model
+    mod.fit(X_train, y_train)
+    # get importance
+    imp = mod.feature_importances_
+    return imp
+#Classifiers for feature importance
+# clf=['Decision Trees','Random Forest']
+# mod_feature = st.sidebar.selectbox('Which model to use?', clf)
+# start_time = timeit.default_timer()
+# if mod_feature=='Decision Trees':
+#     model=etree
+#     importance=feature_sort(model,X_train,y_train)
+# elif mod_feature=='Random Forest':
+#     model=rforest
+#     importance=feature_sort(model,X_train,y_train)
+# elapsed = timeit.default_timer() - start_time
+# st.write('Execution Time for feature selection: %.2f minutes'%(elapsed/60))
+# #Plot of feature importance
+# if st.sidebar.checkbox('Show plot of feature importance'):
+#     plt.bar([x for x in range(len(importance))], importance)
+#     plt.title('Feature Importance')
+#     plt.xlabel('Feature (Variable Number)')
+#     plt.ylabel('Importance')
+#     st.pyplot()
+
+#Import performance metrics, imbalanced rectifiers
+from sklearn.metrics import  confusion_matrix,classification_report,matthews_corrcoef
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import NearMiss
+np.random.seed(42) #for reproducibility since SMOTE and Near Miss use randomizations
+smt = SMOTE()
+nr = NearMiss()
+def compute_performance(model, X_train, y_train,X_test,y_test):
+    start_time = timeit.default_timer()
+    scores = cross_val_score(model, X_train, y_train, cv=3, scoring='accuracy').mean()
+    'Accuracy: ',scores
+    model.fit(X_train,y_train)
+    y_pred = model.predict(X_test)
+    cm=confusion_matrix(y_test,y_pred)
+    'Confusion Matrix: ',cm  
+    # cr=classification_report(y_test, y_pred)
+    # 'Classification Report: ',cr
+    # mcc= matthews_corrcoef(y_test, y_pred)
+    # 'Matthews Correlation Coefficient: ',mcc
+    elapsed = timeit.default_timer() - start_time
+    'Execution Time for performance computation: %.2f minutes'%(elapsed/60)
+#Run different classification models with rectifiers
+if st.sidebar.checkbox('Run a credit card fraud detection model'):
+    
+    alg=['Decision Trees']
+    classifier = st.sidebar.selectbox('Which algorithm?', alg)
+    # rectifier=['SMOTE','Near Miss','No Rectifier']
+    # imb_rect = st.sidebar.selectbox('Which imbalanced class rectifier?', rectifier) 
+    
+    if classifier == 'Decision Trees':
+        model=etree
+        compute_performance(model, X_train, y_train,X_test,y_test)
+
